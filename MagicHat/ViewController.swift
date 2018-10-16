fdfe27da23d6fdd2064a4df8a06d2928e89f82e0
Haley <thatwouldbehaley@gmail.com>
2018-10-16 11:50:23 -0400

    new code

--- MagicHat/ViewController.swift
+++ MagicHat/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
+    private var hasRendered: Bool?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -28,6 +30,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Set the scene to the view
         sceneView.scene = scene*/
+        hasRendered = false
         
         sceneView.scene = SCNScene()
     }
@@ -54,77 +57,30 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         super.didReceiveMemoryWarning()
         // Release any cached data, images, etc that aren't in use.
     }
-
-    // MARK: - ARSCNViewDelegate
-    
-    @IBAction func didTap(_ sender: UITapGestureRecognizer) {
-        // TODO: hit test here
-        let location = sender.location(in: sceneView)
-        let results = sceneView.hitTest(location, types: .existingPlaneUsingExtent)
-        if let result = results.first {
-            placeHat(result)
-        }
-    }
-    
-    private func placeHat() {
-        
-        // Get transform of result
-        let transform = result.worldTransform
-        
-        // Get position from transform (4th column of transformation matrix)
-        let planePosition = SCNVector3Make(transform.columns.3.x, transform.columns.3.y, transform.columns.3.z)
-        
-        // Add door
-        let doorNode = createHatFromScene(planePosition)!
-        sceneView.scene.rootNode.addChildNode(doorNode)
-    }
-    
-    private func createHatFromScene(_ position: SCNVector3) -> SCNNode? {
-        
-        guard let url = Bundle.main.url(forResource: "art.scnassets/hat", withExtension: "scn") else {
-            NSLog("Could not find door scene")
-            return nil
-        }
-        guard let node = SCNReferenceNode(url: url) else { return nil }
-        
-        node.load()
-        
-        // Position scene
-        node.position = position
-        
-        return node
-    }
     
     private var planeNode: SCNNode?
     
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        // Create an SCNNode for a detect ARPlaneAnchor
-        guard let _ = anchor as? ARPlaneAnchor else {
-            return nil
-        }
-        planeNode = SCNNode()
-        return planeNode
-    }
     
-    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
-        // Create an SNCPlane on the ARPlane
-        guard let planeAnchor = anchor as? ARPlaneAnchor else {
-            return
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        if (anchor is ARPlaneAnchor) {
+            if (hasRendered == false) {
+                print("Found Anchor")
+                guard let url = Bundle.main.url(forResource: "art.scnassets/hat", withExtension: "scn") else {
+                    NSLog("Could not find door scene")
+                    return nil
+                }
+                guard let node = SCNReferenceNode(url: url) else { return nil }
+                print("Added Anchor")
+                node.load()
+                //let sphere = SCNSphere(radius: 0.1)
+                //let contentNode = SCNNode(geometry: sphere)
+                planeNode = SCNNode()
+                planeNode?.addChildNode(node)
+                hasRendered = true
+                return planeNode
+            }
         }
-        
-        let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
-        
-        let planeMaterial = SCNMaterial()
-        planeMaterial.diffuse.contents = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 0.3)
-        plane.materials = [planeMaterial]
-        
-        let planeNode = SCNNode(geometry: plane)
-        planeNode.position = SCNVector3Make(planeAnchor.center.x, 0, planeAnchor.center.z)
-        
-        planeNode.transform = SCNMatrix4MakeRotation(-Float.pi / 2, 1, 0, 0)
-        
-        node.addChildNode(planeNode)
-        placeHat()
+        return nil
     }
     
     func session(_ session: ARSession, didFailWithError error: Error) {
